6a3705244dd2292782ff1ca23f1b7713505e0c76
EdgarKorsin-cloud <cuellar171@gmail.com>
2026-08-31 10:59:38 -0600

    Create .gitignor, create enviromental passwords, create procfile

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ from wtforms import PasswordField
 from functools import wraps
 import os
 app = Flask(__name__)
-app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+app.config['SECRET_KEY'] = os.environ.get('FLASK_KEY')
 ckeditor = CKEditor(app)
 Bootstrap5(app)
 login_manager = LoginManager()
@@ -27,7 +27,7 @@ def load_user(user_id):
 
 class Base(DeclarativeBase):
     pass
-app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DB_URI", "sqlite:///posts.db")
 db = SQLAlchemy(model_class=Base)
 db.init_app(app)
 
